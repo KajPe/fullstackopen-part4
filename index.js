17f7cd05aa96997fd249e741b7502ee30e13d852
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-09-29 17:19:12 +0300

    Teht 4.2

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then( () => {
+    console.log('connected to database', process.env.MONGODB_URI)
+  })
+  .catch( err => {
+    console.log(err)
+  })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.logger)
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.error)
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
