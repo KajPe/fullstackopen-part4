7b2820bb74774bd9ce9932647990de1e8811b6da
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-09-29 19:10:26 +0300

    Teht 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,16 +1,37 @@
+// Dummy function ..
 const dummy = (blogs) => {
 
   // Always return 1 ..
   return 1
 }
 
+// Calculate total likes
 const totalLikes = (blogs) => {
   return blogs.reduce(function(sum, blog) {
     return sum + blog.likes
   }, 0)
 }
 
+// Find favorite blog
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  var a = blogs.reduce(function(found, blog) {
+    if ((found[0] === undefined) || (blog.likes > found[0].likes)) {
+      found[0] = blog
+    }
+    return found
+  }, [])
+  return {
+    title: a[0].title,
+    author: a[0].author,
+    likes: a[0].likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
