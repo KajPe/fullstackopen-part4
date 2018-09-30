c92a9afc12fb5c96ad986b80136bf91c597c28a9
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-09-30 14:33:27 +0300

    Teht 4.9 refactored

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -15,14 +15,14 @@ blogsRouter.get('/', async (request, response) => {
   }
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.post('/', async (request, response) => {
+  try {
+    const blog = new Blog(request.body)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (exception) {
+    response.status(500).send({ error: 'Unable to save blog' })
+  }
 })
 
 module.exports = blogsRouter
